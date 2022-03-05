32909cbf29e5c8edba8194df88a20957867a360c
anandarizky12 <anandarizkyrm@gmail.com>
2022-03-05 10:46:20 +0700

    revert

--- server/config/db.config.ts
+++ server/config/db.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "",
-  DB: "elearning",
+  DB: "auth_db",
   dialect: "mysql",
   pool: {
     max: 5,
--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,20 +1,8 @@
-const Classroom = require("./classroom.model");
-
 module.exports = (sequelize: any, Sequelize: any) => {
   const User = sequelize.define("users", {
     username: Sequelize.STRING,
     email: Sequelize.STRING,
     password: Sequelize.STRING,
-    role: Sequelize.STRING,
-    profile: Sequelize.STRING,
-    classroom: Sequelize.JSON,
-  });
-
-  User.hasMany(Classroom, {
-    as: "classrooms",
-    foreignKey: {
-      name: "classroom",
-    },
   });
 
   return User;
