d6b770643b394947a3e1391035be84be1a14bd92
anandarizky12 <rizky.pky@gmail.com>
2022-04-22 18:48:48 +0700

    migrate fixing

--- server/seeders/20220307023023-demo_task.js
+++ server/seeders/20220307023023-demo_task.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert("Tasks", [
+    return queryInterface.bulkInsert("tasks", [
       {
         title:
           "Jelaskan Tentang Penyebab Perang antara Rusia dan Ukraina Secara Singkat ? ",
         description:
           "lorem ipmus si bersama kawan kawan mereka dan aku kan bersepeda keliling desa bersama teman teman dan aku",
         other: "Tugas Nilai Tambahan IPS",
-        deadline: "2023-03-07T23:30:23.000Z",
+        deadline: "2023-03-07",
         classroom_id: 1,
         answer_key:
           "Karna Ukraina menolak untuk bergabung bersama Uni Soviet atau Rusia , dan lebih memilih untuk gabung ke NATO",
@@ -21,7 +21,7 @@ module.exports = {
         description:
           "isi singkat, Jawab pertanyaan ini dengan singkat dan jelas",
         other: "Tugas Nilai Tambahan IPS",
-        deadline: "2023-03-07T23:30:23.000Z",
+        deadline: "2023-03-07",
         classroom_id: 1,
         answer_key:
           "Hak veto adalah sebuah hak khusus untuk membatalkan sebuah keputusan yang telah di buat ",
