e935479bdf26a128ee7fda9682b87de686994032
anandarizky12 <rizky.pky@gmail.com>
2022-05-23 19:48:59 +0700

    data tables

--- client/src/components/Scores/ScoreTable.tsx
+++ client/src/components/Scores/ScoreTable.tsx
@@ -1,23 +1,56 @@
-import { Table, Tag, Space, Avatar, Tooltip, Spin } from "antd";
+import { Spin } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import React from "react";
 import { getAllScores } from "../../actions/task";
 import { useParams } from "react-router-dom";
-import { getStudentsinClassroom } from "../../actions/classroom";
+
 import DynamicError from "../404/DynamicError";
+import DataTable, { TableColumn } from "react-data-table-component";
+import { customStyles, conditionalRowStyles } from "./StylesTable";
 
 export default function ScoreTable() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const { scores } = useSelector((state: any) => state.getAllScores);
   const data = useSelector((state: any) => state.getStudentsInClassroom);
-  const { students } = data;
-  // console.log(scores);
+  const [loading, setLoading] = React.useState(true);
+  const [rows, setRows] = React.useState([]);
 
   React.useEffect(() => {
     dispatch(getAllScores(id));
-    dispatch(getStudentsinClassroom(id));
   }, []);
+
+  type DataRow = {
+    task_title: string;
+    score: string;
+    user: string;
+  };
+  const columns: TableColumn<DataRow>[] = [
+    {
+      name: "Nama Siswa",
+      selector: (row) => row.user,
+      sortable: true,
+    },
+    {
+      name: "Nama Tugas",
+      selector: (row) => row.task_title,
+    },
+    {
+      name: "Nilai",
+      selector: (row) => row.score,
+      sortable: true,
+      style: {
+        fontSize: "17px",
+      },
+    },
+  ];
+
+  React.useEffect(() => {
+    if (scores && scores.data) {
+      setLoading(false);
+      setRows(scores.data);
+    }
+  }, [scores]);
 
   if (!data.isLoading && data.error) {
     return (
@@ -28,67 +61,19 @@ export default function ScoreTable() {
     );
   }
   return (
-    <div className="w-screen overflow-x-scroll">
-      {/* {!data.isLoading && students && scores ? (
-        <>
-          {scores.data.length < 1 && (
-            <div
-              style={{ minHeight: "80vh" }}
-              className="flex justify-center items-center text-xl text-gray-500 "
-            >
-              Belum ada data nilai pada kelas ini
-            </div>
-          )}
-          <table width={"100%"} className="  ">
-            <thead>
-              <tr>
-                <th className="h-20 min-w-96"></th>
-                {scores.data.map((task: any) => (
-                  <th key={task.id} className="border min-w-96">
-                    <Tooltip
-                      placement="bottom"
-                      className="hover : cursor-pointer"
-                      title={task.Task.title}
-                    >
-                      {task.Task.title.slice(0, 18) + " . . . "}
-                    </Tooltip>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {students.data.map((student: any) => (
-                <tr key={student.id} className="text-center">
-                  <td className="border w-52">
-                    <div className="flex items-center p-5 ">
-                      <Avatar src={student.User.profile} size="large" />
-                      <div className="ml-2">{student.User.name}</div>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-              {scores.data.map((student: any) => (
-                <tr key={student.id} className="text-center">
-                  <td className="border text-xl text-gray-500">
-                    {student.score}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
-      ) : (
-        <div
-          style={{
-            minHeight: "90vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Spin size={"large"} />
-        </div>
-      )} */}
+    <div className="w-full flex flex-col mt-12 items-center justify-center">
+      <div className="w-4/6 border p-5 shadow-md">
+        <DataTable
+          title="Daftar Nilai Siswa"
+          columns={columns}
+          data={rows}
+          pagination
+          progressPending={loading}
+          defaultSortFieldId={1}
+          customStyles={customStyles}
+          conditionalRowStyles={conditionalRowStyles}
+        />
+      </div>
     </div>
   );
 }
